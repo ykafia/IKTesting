da7f278556e16ef4ae893ecfa1257b98ce697520
Youness KAFIA <kafia.youness@hotmail.fr>
2021-07-29 12:44:22 +0200

    update on bone look at

--- IKTesting/IKSystem/IKComponent.cs
+++ IKTesting/IKSystem/IKComponent.cs
@@ -69,85 +69,22 @@ namespace IKTesting
             var no = sk.NodeTransformations[i];
             var p = Matrix.Invert(sk.NodeTransformations[no.ParentIndex].WorldMatrix);
            
+            // Foreach Bone strings that needs targets
             foreach(var (n,e) in BoneToTarget)
             {
-                var noWP = no.WorldMatrix.TranslationVector;
-                var cpos = Vector3.Transform(sk.NodeTransformations[i+1].WorldMatrix.TranslationVector,p).XYZ();
-                var localChildPos = Vector3.Transform(sk.NodeTransformations[i+1].WorldMatrix.TranslationVector, p).XYZ();
-                var localTargetPos = Vector3.Transform(e.Transform.Position,p).XYZ();
-                
-                var rot = Quaternion.RotationMatrix(Matrix.LookAtRH(no.Transform.Position,localTargetPos,no.LocalMatrix.Up));
-                var cor = Quaternion.BetweenDirections(no.LocalMatrix.Forward, cpos - no.Transform.Position);
-                
-                sk.NodeTransformations[i].Transform.Rotation = rot;
-                
+                // Compute Fabrik
+
+                // Compute rotations for each points of the targets              
             }
             
         }
-        public static Quaternion LookAt(Vector3 forward, Vector3 up)
+        public static Quaternion BoneLookAt(Vector3 childWPos, Vector3 targetPos, Vector3 nodeLocalPos, Matrix nodeLocal, Matrix parentWorldInverted)
         {
-            // Taken from http://answers.unity.com/answers/467979/view.html
+            var cpos = Vector3.Transform(childWPos,parentWorldInverted).XYZ();
+            var tpos = Vector3.Transform(targetPos,parentWorldInverted).XYZ();
+            var forward = Quaternion.RotationMatrix(Matrix.LookAtRH(nodeLocalPos, tpos,nodeLocal.Up));
             forward.Normalize();
-        
-            Vector3 vector = Vector3.Normalize(forward);
-            Vector3 vector2 = Vector3.Normalize(Vector3.Cross(up, vector));
-            Vector3 vector3 = Vector3.Cross(vector, vector2);
-            var m00 = vector2.X;
-            var m01 = vector2.Y;
-            var m02 = vector2.Z;
-            var m10 = vector3.X;
-            var m11 = vector3.Y;
-            var m12 = vector3.Z;
-            var m20 = vector.X;
-            var m21 = vector.Y;
-            var m22 = vector.Z;
-        
-        
-            float num8 = m00 + m11 + m22;
-            var quaternion = new Quaternion();
-            if (num8 > 0f)
-            {
-                var num = (float)Math.Sqrt(num8 + 1f);
-                quaternion.W = num * 0.5f;
-                num = 0.5f / num;
-                quaternion.X = (m12 - m21) * num;
-                quaternion.Y = (m20 - m02) * num;
-                quaternion.Z = (m01 - m10) * num;
-                return quaternion;
-            }
-            if ((m00 >= m11) && (m00 >= m22))
-            {
-                var num7 = (float)Math.Sqrt(1f + m00 - m11 - m22);
-                var num4 = 0.5f / num7;
-                quaternion.X = 0.5f * num7;
-                quaternion.Y = (m01 + m10) * num4;
-                quaternion.Z = (m02 + m20) * num4;
-                quaternion.W = (m12 - m21) * num4;
-                return quaternion;
-            }
-            if (m11 > m22)
-            {
-                var num6 = (float)Math.Sqrt(1f + m11 - m00 - m22);
-                var num3 = 0.5f / num6;
-                quaternion.X = (m10+ m01) * num3;
-                quaternion.Y = 0.5f * num6;
-                quaternion.Z = (m21 + m12) * num3;
-                quaternion.W = (m20 - m02) * num3;
-                return quaternion; 
-            }
-            var num5 = (float)Math.Sqrt(1f + m22 - m00 - m11);
-            var num2 = 0.5f / num5;
-            quaternion.X = (m20 + m02) * num2;
-            quaternion.Y = (m21 + m12) * num2;
-            quaternion.Z = 0.5f * num5;
-            quaternion.W = (m01 - m10) * num2;
-            return quaternion;
- 
-        }
-        public static Quaternion BDir(Vector3 sourceDir, Vector3 targetDir)
-        {
-            var t = Quaternion.BetweenDirections(sourceDir,targetDir).YawPitchRoll;
-            return Quaternion.RotationYawPitchRoll(t.X,t.Y,0);
+            return forward * Quaternion.Normalize(Quaternion.Invert(Quaternion.BetweenDirections(nodeLocal.Forward,cpos - nodeLocalPos)));
         }
         
 
